b5501071b4937f1cae07f242540479e1414b0281
Noctoid <i@noctoid.com>
2018-10-29 02:59:57 -0700

    clean unused include :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
-#include <iostream>
 #include <string>
 #include "UI/TextUserInterface/TextUserInterface.hpp"
-#include "TechnicalServices/AuthorizationVendorConnector/AuthorizationVendorConnector.hpp"
 
 int main() {
     TextUserInterface tui;
