672114b9c88ea10907aab19c0ae04e39c46fea69
Noctoid <i@noctoid.com>
2018-10-29 02:54:26 -0700

    added main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+#include "UI/TextUserInterface/TextUserInterface.hpp"
+#include "TechnicalServices/AuthorizationVendorConnector/AuthorizationVendorConnector.hpp"
+
+int main() {
+    TextUserInterface tui;
+    return 0;
+}
