def6f424f4b0ad52cb99ea19bccb2a71792f5b21
Noctoid <i@noctoid.com>
2018-10-30 12:30:39 -0700

    added logging to AuthorizationHandler

--- src/Domain/AuthorizationHandler/AuthorizationHandler.cpp
+++ src/Domain/AuthorizationHandler/AuthorizationHandler.cpp
@@ -1,11 +1,21 @@
 #include <string>
+#include <fstream>
+#include <vector>
 
 #include "AuthorizationHandler.hpp"
 #include "../../TechnicalServices/AuthorizationVendorConnector/AuthorizationVendorConnector.hpp"
+#include "../../TechnicalServices/Logger/Logger.hpp"
 
 AuthorizationHandler::AuthorizationHandler() {}
 
 int AuthorizationHandler::authenticate(int userid, std::string password) {
     AuthorizationVendorConnector auth("auth.vendor.com/api/v15/");
-    return auth.authenticateUser(userid, password);
+    int userid_logged_in = auth.authenticateUser(userid, password);
+
+    // create logger object and log the login event
+    Logger logger("test.log");
+    std::string msg;
+    msg = "User - " + std::to_string(userid_logged_in) + " logged in.";
+    logger.log("LOGIN", userid_logged_in, msg);
+    return userid_logged_in;
 }
