10ffac8905996e8c69b9f9aedbfee653ba8ef52a
Noctoid <i@noctoid.com>
2018-10-30 13:27:10 -0700

    added logging for OrderHandler

--- src/Domain/OrderHandler/OrderHandler.cpp
+++ src/Domain/OrderHandler/OrderHandler.cpp
@@ -1,7 +1,10 @@
 #include <string>
+#include <vector>
+#include <fstream>
 #include <sqlite3.h>
 #include "OrderHandler.hpp"
 #include "../../TechnicalServices/DatabaseConnector/DatabaseConnector.hpp"
+#include "../../TechnicalServices/Logger/Logger.hpp"
 
 #include <stdlib.h>
 #include <time.h>
@@ -19,6 +22,12 @@ int OrderHandler::createOrder(int UPC, int quantity, int payid) {
     int orderid = this->getRandomNumber(1, 1000000);
     DatabaseConnector db("test.db");
     db.OrderCreate(orderid, current_userid, UPC, quantity, payid);
+
+    // log order event
+    Logger logger("test.log");
+    std::string msg = "Order " + std::to_string(orderid) + " created by Customer-" + std::to_string(current_userid) + " with PayID-"+std::to_string(payid);
+    logger.log("USER", current_userid, msg);
+
     return orderid;
 }
 
@@ -26,5 +35,11 @@ int OrderHandler::createPayment(std::string method, std::string amount, int refu
     int payid = this->getRandomNumber(1, 1000000);
     DatabaseConnector db("test.db");
     db.PaymentCreate(payid, current_userid, method, amount, refund);
+
+    // log this payment
+    Logger logger("test.log");
+    std::string msg = "Payment "+std::to_string(payid)+" created by Customer-"+std::to_string(current_userid)+" with amount $-"+amount;
+    logger.log("USER", current_userid, msg);
+
     return payid;
 }
