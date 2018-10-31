d368f360464a78f060f9e984274983feb9262c6c
Noctoid <i@noctoid.com>
2018-10-31 02:05:20 -0700

    added new users

--- src/TechnicalServices/AuthorizationVendorConnector/AuthorizationVendorConnector.cpp
+++ src/TechnicalServices/AuthorizationVendorConnector/AuthorizationVendorConnector.cpp
@@ -10,13 +10,31 @@ int AuthorizationVendorConnector::authenticateUser(int UserID, std::string passw
 	if (UserID == 1 && !(password.compare("qwer1234asdf")) ) {
         return 0;
     }
-    if (UserID == 100 && !(password.compare("qwer1234asdf")) ) {
+    else if (UserID == 100 && !(password.compare("qwer1234asdf")) ) {
         return 100;
     }
-    if (UserID == 200 && !(password.compare("qwer1234asdf")) ) {
+    else if (UserID == 200 && !(password.compare("qwer1234asdf")) ) {
         return 200;
     }
-    if (UserID == 1000 && !(password.compare("qwer1234asdf")) ) {
+    else if (UserID == 1000 && !(password.compare("qwer1234asdf")) ) {
+        return 1000;
+    }
+    else if (UserID == 1001 && !(password.compare("A904#!^wWcIDRL%^rp&Tv%@QIjOWYYk$14i")) ) {
+        return 0;
+    }
+    else if (UserID == 2001 && !(password.compare("$Fak%iXH%s@8PtA$WOjA")) ) {
+        return 100;
+    }
+    else if (UserID == 3001 && !(password.compare("Rvzm4#PfX0t2rzE$tJs1")) ) {
+        return 200;
+    }
+    else if (UserID == 647238 && !(password.compare("vZeI0Jd#!9#NtPbEe3Yn")) ) {
+        return 1000;
+    }
+    else if (UserID == 837502 && !(password.compare("!hM%41g*OyYs^G^utGEh")) ) {
+        return 1000;
+    }
+    else if (UserID == 726941 && !(password.compare("FpIp&%8bWAqQmaw$UAAz")) ) {
         return 1000;
     }
     return -1;
